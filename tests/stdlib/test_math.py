dc0aef82060ba8753e0ab745ab608bb533b27dc1
Adam Ross <14985050+R055A@users.noreply.github.com>
2020-09-14 15:57:33 +0100

    Add math module TranspileTestCase test Implement test class Test math.floor with multiple argument types: int, float, bool, str, None

---
+++ tests/stdlib/test_math.py
@@ -0,0 +1,72 @@
+from unittest import expectedFailure
+
+from ..utils import TranspileTestCase, NotImplementedToExpectedFailure
+
+
+class MathModuleTests(TranspileTestCase, NotImplementedToExpectedFailure):
+    """
+    Testing of Python3 math module methods with TranspileTestCase:
+        * math.floor
+        *
+    """
+
+    #######################################################
+    # math.floor
+    @expectedFailure
+    def test_math_floor_None_argument(self):
+        self.assertCodeExecution("""
+            from math import floor
+            test_arg = None
+            print(floor(test_arg))
+            """)
+
+    @expectedFailure
+    def test_math_floor_str_argument(self):
+        self.assertCodeExecution("""
+            from math import floor
+            test_arg = "str"
+            print(floor(test_arg))
+            """)
+
+    @expectedFailure
+    def test_math_floor_inf_argument(self):
+        self.assertCodeExecution("""
+            from math import floor, inf
+            test_arg = inf
+            print(floor(test_arg))
+
+            test_arg = -inf
+            print(floor(test_arg))
+            """)
+
+    def test_math_floor_int_argument(self):
+        self.assertCodeExecution("""
+            from math import floor
+            test_arg = 2
+            print(floor(test_arg))
+
+            test_arg = -2
+            print(floor(test_arg))
+            """)
+
+    def test_math_floor_float_argument(self):
+        self.assertCodeExecution("""
+            from math import floor
+            test_arg = 16.66
+            print(floor(test_arg))
+
+            test_arg = -16.66
+            print(floor(test_arg))
+            """)
+
+    def test_math_floor_bool_argument(self):
+        self.assertCodeExecution("""
+            from math import floor
+            test_arg = True
+            print(floor(test_arg))
+
+            test_arg = False
+            print(floor(test_arg))
+            """)
+
+    #######################################################
