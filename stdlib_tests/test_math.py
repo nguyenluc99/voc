6564430ca145f1f0d55b9593dbe26b0b6c59f55d
Adam Ross <14985050+R055A@users.noreply.github.com>
2020-09-17 09:33:28 +0100

    Add tests to stdlib_tests for int, bool, str and None

--- stdlib_tests/test_math.py
+++ stdlib_tests/test_math.py
@@ -2,7 +2,17 @@ from math import floor
 print(floor(16.66))  # expected output: 16
 print(floor(-19.2))  # expected output: 20
 
-#print(floor(float("Infinity")))  # expected output: OverflowError: Python cannot convert float infinity to integer
-#print(floor(float("-Infinity")))  # expected output: OverflowError: Python cannot convert float infinity to integer
+print(floor(11))  # expected output: 11
+print(floor(-33))  # expected output: -33
 
-#print(floor(float("nan")))  #  expected output: ValueError: Python cannot convert float NaN to integer
+print(floor(True))  # expected output: 1
+print(floor(False))  #expected output: 0
+
+#print(floor(float("Infinity")))  # expected output: OverflowError: Cannot convert float infinity to integer
+#print(floor(float("-Infinity")))  # expected output: OverflowError: Cannot convert float infinity to integer
+
+#print(floor(float("nan")))  #  expected output: ValueError: Cannot convert float NaN to integer
+
+#print(floor("str"))  # expected output: TypeError: A float is required, not: str
+
+#print(floor(None))  # expected output: TypeError: A float is required, not: NoneType
