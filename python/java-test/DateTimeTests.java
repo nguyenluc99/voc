557bb7b807441d1cad046a5393bdb17da4431978
ponterik <pontus.z.eriksson@outlook.com>
2020-09-25 17:34:24 +0200

    test are not passing yet

--- python/java-test/DateTimeTests.java
+++ python/java-test/DateTimeTests.java
@@ -7,6 +7,9 @@ import org.python.types.Str;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDateTime;    
+
 public class DateTimeTests {
     @Test
     public void test_construction() {
@@ -17,5 +20,20 @@ public class DateTimeTests {
         DateTime date = new DateTime(args, Collections.emptyMap());
         System.out.println(date.__str__());
         assertEquals(date.__str__(), new Str("0001-02-03 00:00:00"));
+    }
+    @Test
+    public void test_today() {
+        
+        Int[] args = {Int.getInt(1), Int.getInt(2), Int.getInt(3)};
+        System.out.println(Collections.emptyMap());
+        DateTime date = new DateTime(args, Collections.emptyMap());
+        
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS");
+        LocalDateTime now = LocalDateTime.now(); 
+        System.out.println(date.__str__());
+        //Str date_str = new Str(date.today());
+        org.python.Object date2 = date.today();
+        String date3 = (String) date2.__str__().toJava();
+        assertEquals(date3, new Str(dtf.format(now)));
     }
 }
