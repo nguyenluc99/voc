bdbb975e78bd487029999ffa3443d34cd920a9ed
ponterik <pontus.z.eriksson@outlook.com>
2020-09-30 20:49:42 +0200

    Removed the millisecond testing part of the today test

--- python/java-test/DateTimeTests.java
+++ python/java-test/DateTimeTests.java
@@ -137,13 +137,13 @@ public class DateTimeTests {
     @Test
     public void test_today() {
         //This methods return value is only dependent on the current day so one test is enough. 
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SS");
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         LocalDateTime now = LocalDateTime.now(); 
 
 
         org.python.Object python_date_full = DateTime.today();
         String java_date_full = (String) python_date_full.__str__().toJava();
-        String java_date_reduced = java_date_full.substring(0,java_date_full.length()-4);
+        String java_date_reduced = java_date_full.substring(0,java_date_full.length()-7);
         assertEquals(new Str(java_date_reduced), new Str(dtf.format(now)));
     }
 
@@ -213,71 +213,6 @@ public class DateTimeTests {
         String java_date_full = (String) python_date_yymmdd.__str__().toJava();
         assertEquals(new Str(dtf.format(aDate)), new Str(java_date_full));
     }
-
-        
-        /*
-        args = Arrays.copyOf(args, args.length-1);
-        HashMap<String, org.python.Object> kwargs = new HashMap<String, org.python.Object>();
-        kwargs.put("day", Int.getInt(3));
-
-        date = new DateTime(args, kwargs);
-        assertEquals(date.__str__(), new Str("0001-02-03 00:00:00"));
-
-        args = Arrays.copyOf(args, args.length-1);
-        kwargs.put("month", Int.getInt(2));
-
-        date = new DateTime(args, kwargs);
-        assertEquals(date.__str__(), new Str("0001-02-03 00:00:00"));
-
-        args = Arrays.copyOf(args, args.length-1);
-        kwargs.put("year", Int.getInt(1));
-
-        date = new DateTime(args, kwargs);
-        assertEquals(date.__str__(), new Str("0001-02-03 00:00:00"));
-
-        args = Arrays.copyOf(args, args.length+3);
-        args[0] = Int.getInt(11);
-        args[1] = Int.getInt(12);
-        args[2] = Int.getInt(13);
-        date = new DateTime(args, Collections.emptyMap());
-        assertEquals(date.__str__(), new Str("0011-12-13 00:00:00"));
-
-        args[0] = Int.getInt(111);
-        date = new DateTime(args, Collections.emptyMap());
-        assertEquals(date.__str__(), new Str("0111-12-13 00:00:00"));
-
-        args[0] = Int.getInt(1111);
-        date = new DateTime(args, Collections.emptyMap());
-        assertEquals(date.__str__(), new Str("1111-12-13 00:00:00"));
-
-        args = Arrays.copyOf(args, args.length+3);
-        args[3] = Int.getInt(0);
-        args[4] = Int.getInt(0);
-        args[5] = Int.getInt(0);
-        date = new DateTime(args, Collections.emptyMap());
-        assertEquals(date.__str__(), new Str("1111-12-13 00:00:00"));
-
-        args = Arrays.copyOf(args, args.length+1);
-        args[6] = Int.getInt(0);
-        date = new DateTime(args, Collections.emptyMap());
-        assertEquals(date.__str__(), new Str("1111-12-13 00:00:00"));
-
-        args = Arrays.copyOf(args, args.length-1);
-        args[0] = Int.getInt(9999);
-        args[1] = Int.getInt(12);
-        args[2] = Int.getInt(31);
-        args[3] = Int.getInt(23);
-        args[4] = Int.getInt(59);
-        args[5] = Int.getInt(59);
-        date = new DateTime(args, Collections.emptyMap());
-        assertEquals(date.__str__(), new Str("9999-12-31 23:59:59"));
-
-        args = Arrays.copyOf(args, args.length+1);
-        args[6] = Int.getInt(999999);
-        date = new DateTime(args, Collections.emptyMap());
-        assertEquals(date.__str__(), new Str("9999-12-31 23:59:59.999999"));
-    */
-
 
     @Test
     public void test_weekday_now() {
