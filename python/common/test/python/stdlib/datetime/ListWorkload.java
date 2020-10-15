628d0649852dc68d7d657f0211c94442732a0fb6
nguyenluc99 <yeuma99@gmail.com>
2020-10-15 13:46:54 +0200

    add workload 2

--- python/common/test/python/stdlib/datetime/ListWorkload.java
+++ python/common/test/python/stdlib/datetime/ListWorkload.java
@@ -2,8 +2,7 @@ package test.python.stdlib.datetime;
 
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.Random;
 
 public class ListWorkload {
@@ -14,22 +13,8 @@ public class ListWorkload {
         ListWorkload.__setitem__Workload();
         ListWorkload.countWorkload();
 
-        Integer sizeOfList = Integer.valueOf(10000);
-        ArrayList arrayList1 = new ArrayList();
-        ArrayList arrayList2 = new ArrayList();
-        ArrayList arrayList3 = new ArrayList();
-        Random rd = new Random();
-        for (int i = 0; i < sizeOfList.intValue(); i++) {
-            Integer temp = rd.nextInt();
-            arrayList1.add(temp);
-            arrayList2.add(temp);
-            arrayList3.add(temp);
-        }
-        // ascending order
-        ListWorkload.testBubbleSort(arrayList1);
-        ListWorkload.testComparator(arrayList2);
-        ListWorkload.testCollection(arrayList3);
-
+        ListWorkload.workload1Luc();
+        ListWorkload.workload2Luc();
     }
 
     // Remove at beginning (good for linked-list, bad for array-list)
@@ -83,35 +68,74 @@ public class ListWorkload {
         }
     }
 
-    static class MyCompare implements Comparator<Integer> {
-        @Override
-        public int compare(Integer c1, Integer c2) {
-            if (c1 == c2) return 0;
-            if (c1 > c2) {
-                return 1;
-            }
-            return -1;
+    public static void addElementToArrayList(Integer sizeOfList, ArrayList<Integer> arrayList, Random rd) {
+        for (int i = 0; i < sizeOfList; i++) {
+            arrayList.add(rd.nextInt(Integer.MAX_VALUE) % (i + 1), i);
         }
     }
 
-    public static void testBubbleSort(ArrayList<Integer> arr) {
-        Integer len = arr.size();
-        for (int i = 0; i < len; i++) {
-            for (int j = 0; j < i; j++) {
-                if (arr.get(i).intValue() < arr.get(j).intValue()) {
-                    Integer temp = arr.get(i);
-                    arr.set(i, arr.get(j));
-                    arr.set(j, temp);
-                }
+    public static void addElementToLinkedList(Integer sizeOfList, LinkedList<Integer> linkedList, Random rd) {
+        for (int i = 0; i < sizeOfList; i++) {
+            linkedList.add(rd.nextInt(Integer.MAX_VALUE) % (i + 1), i);
+        }
+    }
+
+    public static void removeSubListArrayList(Integer iteration, Integer sizeOfList, ArrayList arrayList, Random rd) {
+        int first, last;
+
+        for (int i = 0; i < iteration; i++) {
+            first = rd.nextInt(Integer.MAX_VALUE) % (sizeOfList + 1);
+            last = rd.nextInt(Integer.MAX_VALUE) % (sizeOfList + 1);
+            if (first < last) {
+                arrayList.subList(first, last).clear();
+                sizeOfList = sizeOfList + first - last;
+            } else {
+                arrayList.subList(last, first).clear();
+                sizeOfList = sizeOfList + last - first;
             }
         }
     }
 
-    public static void testComparator(ArrayList<Integer> arr) {
-        arr.sort(new ListWorkload.MyCompare());
+    public static void removeSubListLinkedList(Integer iteration, Integer sizeOfList, LinkedList linkedList, Random rd) {
+        int first, last;
+
+        for (int i = 0; i < iteration; i++) {
+            first = rd.nextInt(Integer.MAX_VALUE) % (sizeOfList + 1);
+            last = rd.nextInt(Integer.MAX_VALUE) % (sizeOfList + 1);
+            if (first < last) {
+                linkedList.subList(first, last).clear();
+                sizeOfList = sizeOfList + first - last;
+            } else {
+                linkedList.subList(last, first).clear();
+                sizeOfList = sizeOfList + last - first;
+            }
+        }
     }
 
-    public static void testCollection(ArrayList<Integer> arr) {
-        Collections.sort(arr, new ListWorkload.MyCompare());
+    public static void workload1Luc() {
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+        Random rd = new Random();
+
+        int sizeOfList = 3000;
+
+        ListWorkload.addElementToLinkedList(sizeOfList, linkedList, rd);
+        ListWorkload.addElementToArrayList(sizeOfList, arrayList, rd);
     }
+
+    public static void workload2Luc() {
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+        Random rd = new Random();
+        int sizeOfList = 100000;
+        int iteration = 5000;
+
+        for (int i = 0; i < sizeOfList; i++) {
+            arrayList.add(i);
+            linkedList.add(i);
+        }
+        ListWorkload.removeSubListArrayList(iteration, sizeOfList, arrayList, rd);
+        ListWorkload.removeSubListLinkedList(iteration, sizeOfList, linkedList, rd);
+    }
+
 }
